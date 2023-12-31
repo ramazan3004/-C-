c5d019e5f2ab16abd61269e590e55c3d037409da
ramaz3004 <ramaz3004@mail.ru>
2023-12-31 04:40:41 +0300

    добавлена задача #3 показ наибольшей цифры числа

---
+++ Дом.зад3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 3: Напишите программу, которая принимает
+// на вход целое число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+
+
+Console.WriteLine("Введите число из отрезка 10, 99");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = number % 10;
+int b = number / 10;
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
