760bf67f9ce5b5cea03dd5afe48ea4c2688f971b
ramaz3004 <ramaz3004@mail.ru>
2023-12-31 05:01:13 +0300

    задание #4 вывод цифр через запятую

---
+++ Дом.зад4/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+
+Console.WriteLine("Введите натуральное число n");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 10)
+{
+Console.WriteLine(N);
+}
+else
+{
+while (N > 0)
+{
+int currentDigit = N % 10;
+N /= 10;
+{
+Console.Write(currentDigit + ", ");
+}
+}
+}
