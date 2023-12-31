6399f74df248597d8fd2b10fdcade73a70524501
ramaz3004 <ramaz3004@mail.ru>
2023-12-31 04:24:34 +0300

    Выполнил второе домашнее задание

---
+++ Дом.зад2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 2: Напишите программу, которая принимает
+// на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠
+// 0 и выдаёт номер координатной четверти плоскости,
+// в которой находится эта точка
+
+// 2, 3 => 1
+// -5, 3 => 2
+// -3, -2 => 3
+// 4, -2 => 4
+
+Console.WriteLine("Введите координаты X");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты Y");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("1");
+}
+if (x < 0 && y < 0)
+{
+    Console.WriteLine("3");
+}
+if (x > 0 && y < 0)
+{
+    Console.WriteLine("4");
+}
+if (x < 0 && y > 0)
+{
+    Console.WriteLine("2");
+}
